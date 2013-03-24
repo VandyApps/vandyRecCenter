57919a53f4bc9cb57ce576a9884f704984303a0d
Brendan McNamara <brendan@streampiece.com>
2013-03-23 21:05:13 -0500

    Initial Commit

---
+++ vandyRecCenter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  vandyRecCenter
+//
+//  Created by Brendan McNamara on 3/23/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ vandyRecCenter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  vandyRecCenter
+//
+//  Created by Brendan McNamara on 3/23/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
