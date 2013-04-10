b96ff2ece280cb6d80a68327615fbfa351d932ee
Brendan McNamara <brendan@streampiece.com>
2013-04-10 09:20:57 -0500

    Fixes bugs in the method dayOfTheWeekForIndex in MyDateClass

--- vandyRecCenter/NSDate-MyDateClass.m
+++ vandyRecCenter/NSDate-MyDateClass.m
@@ -54,15 +54,17 @@
         case 1:
             return @"Monday";
         case 2:
-            return @"Tueday";
+            return @"Tuesday";
         case 3:
             return @"Wednesday";
         case 4:
             return @"Thursday";
         case 5:
             return @"Friday";
-        default: //when the value is 6
+        case 6: 
             return @"Saturday";
+        default:
+            return nil;
     }
 }
 
