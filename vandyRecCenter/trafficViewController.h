568c54873cabc80c7ebed82f84aaeb41871f4613
Brendan McNamara <brendan@streampiece.com>
2013-04-13 12:16:26 -0500

    adds view controller for the traffic tab

---
+++ vandyRecCenter/trafficViewController.h
@@ -0,0 +1,13 @@
+//
+//  trafficViewController.h
+//  vandyRecCenter
+//
+//  Created by Brendan McNamara on 4/13/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface trafficViewController : UIViewController
+
+@end
---
+++ vandyRecCenter/trafficViewController.m
@@ -0,0 +1,13 @@
+//
+//  trafficViewController.m
+//  vandyRecCenter
+//
+//  Created by Brendan McNamara on 4/13/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import "trafficViewController.h"
+
+@implementation trafficViewController
+
+@end
