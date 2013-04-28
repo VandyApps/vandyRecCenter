8590467250c5f4112bea0abe24f34a051058a8c3
Brendan McNamara <brendan@streampiece.com>
2013-04-28 12:48:43 -0500

    adds trafficModel file

---
+++ vandyRecCenter/trafficModel.h
@@ -0,0 +1,13 @@
+//
+//  trafficModel.h
+//  
+//
+//  Created by Brendan McNamara on 4/28/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "GraphModel.h"
+@interface trafficModel : NSObject
+
+@end
---
+++ vandyRecCenter/trafficModel.m
@@ -0,0 +1,13 @@
+//
+//  trafficModel.m
+//  
+//
+//  Created by Brendan McNamara on 4/28/13.
+//
+//
+
+#import "trafficModel.h"
+
+@implementation trafficModel
+
+@end
