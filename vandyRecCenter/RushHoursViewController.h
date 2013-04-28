8f23aa28a15607ee0ef4f5427015273a736b9976
Brendan McNamara <brendan@streampiece.com>
2013-04-28 12:20:06 -0500

    adds subclass for rush hours

---
+++ vandyRecCenter/RushHoursViewController.h
@@ -0,0 +1,13 @@
+//
+//  RushHoursViewController.h
+//  vandyRecCenter
+//
+//  Created by Brendan McNamara on 4/28/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RushHoursViewController : UIViewController
+
+@end
