814841ddb9644e59038cab408548e677da5bd7d1
Brendan McNamara <brendan@streampiece.com>
2013-05-16 21:49:05 -0500

    adds tab bar navigation delegate protocol

---
+++ vandyRecCenter/TabBarNavigationDelegate.h
@@ -0,0 +1,13 @@
+//
+//  TabBarNavigationDelegate.h
+//  vandyRecCenter
+//
+//  Created by Brendan McNamara on 5/16/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol TabBarNavigationDelegate <NSObject>
+
+@end
