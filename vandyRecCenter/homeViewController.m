c15abd2f0f46077c961cb213dde53cf29df4f2f5
Brendan McNamara <brendan@streampiece.com>
2013-05-16 23:38:39 -0500

    shows and hides scrollers

--- vandyRecCenter/homeViewController.m
+++ vandyRecCenter/homeViewController.m
@@ -53,23 +53,35 @@
 
 - (void) setScrollViewSubviews {
     [self removeAllViewsFromScrollView];
-    [self addPageToScrollViewAtIndex: 0];
-    [self addPageToScrollViewAtIndex: 1];
+    [self addPageToScrollViewAtIndex: 0 hideScrollersInPortraitOrientation: NO];
+    [self addPageToScrollViewAtIndex: 1 hideScrollersInPortraitOrientation: NO];
 }
 
 
-- (void) addPageToScrollViewAtIndex: (NSUInteger) index {
+//hideScrollers determines if the scrollers are to be hidden when in portriat orientation
+//scrollers are always hidden in landscape orientation and should be hidden
+//in portrait orientation only when there is only a single page to display
+- (void) addPageToScrollViewAtIndex: (NSUInteger) index hideScrollersInPortraitOrientation: (BOOL) hideScrollers{
     
     CGRect frameOfPage;
     if (UIInterfaceOrientationIsPortrait([[UIDevice currentDevice] orientation])) {
         
         frameOfPage = CGRectMake((self.scrollView.frame.size.width - DIMENSIONS_OF_PAGE_PORTRAIT)/2.0 + (index *self.view.frame.size.width), (self.scrollView.frame.size.height - DIMENSIONS_OF_PAGE_PORTRAIT)/2.0, DIMENSIONS_OF_PAGE_PORTRAIT, DIMENSIONS_OF_PAGE_PORTRAIT);
+        NSLog(@"In portrait mode");
+        if (hideScrollers) {
+            self.leftScroller.hidden = YES;
+            self.rightScroller.hidden = YES;
+        } else {
+            self.leftScroller.hidden = NO;
+            self.rightScroller.hidden = NO;
+        }
         
     } else {
         
         //for now, add height translation in landscape orientation
         frameOfPage = CGRectMake((self.scrollView.frame.size.width - DIMENSIONS_OF_PAGE_LANDSCAPE)/2.0 + (index *self.view.frame.size.width), (self.scrollView.frame.size.height - DIMENSIONS_OF_PAGE_LANDSCAPE)/2.0 + 10, DIMENSIONS_OF_PAGE_LANDSCAPE, DIMENSIONS_OF_PAGE_LANDSCAPE);
-        
+        self.leftScroller.hidden = YES;
+        self.rightScroller.hidden = YES;
         
     }
     UIView* page = [[UIView alloc] initWithFrame: frameOfPage];
