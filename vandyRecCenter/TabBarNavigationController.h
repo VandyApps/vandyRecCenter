134ae8a6796bceac3598839e646a8056839ee019
Brendan McNamara <brendan@streampiece.com>
2013-05-16 21:47:45 -0500

    adds tab bar navigation controller class

---
+++ vandyRecCenter/TabBarNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarNavigationController.h
+//  vandyRecCenter
+//
+//  Created by Brendan McNamara on 5/16/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarNavigationController : UINavigationController
+
+@end
---
+++ vandyRecCenter/TabBarNavigationController.m
@@ -0,0 +1,38 @@
+//
+//  TabBarNavigationController.m
+//  vandyRecCenter
+//
+//  Created by Brendan McNamara on 5/16/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import "TabBarNavigationController.h"
+
+@interface TabBarNavigationController ()
+
+@end
+
+@implementation TabBarNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
