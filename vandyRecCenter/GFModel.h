09168af0fffc8473c849152bcc1346f6dbf2d8c5
Brendan McNamara <brendan@streampiece.com>
2013-05-26 12:32:16 -0500

    adds GFModel class

---
+++ vandyRecCenter/GFModel.h
@@ -0,0 +1,13 @@
+//
+//  GFModel.h
+//  
+//
+//  Created by Brendan McNamara on 5/26/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GFModel : NSObject
+
+@end
---
+++ vandyRecCenter/GFModel.m
@@ -0,0 +1,13 @@
+//
+//  GFModel.m
+//  
+//
+//  Created by Brendan McNamara on 5/26/13.
+//
+//
+
+#import "GFModel.h"
+
+@implementation GFModel
+
+@end
