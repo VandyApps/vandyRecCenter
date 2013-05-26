492b654639d853754e9ebde1ef6cd3c74e9f8dfb
Brendan McNamara <brendan@streampiece.com>
2013-05-26 11:24:49 -0500

    adds GFViewController

---
+++ vandyRecCenter/GFViewController.h
@@ -0,0 +1,13 @@
+//
+//  GFViewController.h
+//  vandyRecCenter
+//
+//  Created by Brendan McNamara on 5/26/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GFViewController : UIViewController
+
+@end
