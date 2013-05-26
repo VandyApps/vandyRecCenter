167c1f5ff444441a56651c97c21b19def2fdcc8e
Brendan McNamara <brendan@streampiece.com>
2013-05-26 16:23:34 -0500

    creates TimeString class

---
+++ vandyRecCenter/TimeString.h
@@ -0,0 +1,13 @@
+//
+//  TimeString.h
+//  vandyRecCenter
+//
+//  Created by Brendan McNamara on 5/26/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TimeString : NSObject
+
+@end
---
+++ vandyRecCenter/TimeString.m
@@ -0,0 +1,13 @@
+//
+//  TimeString.m
+//  vandyRecCenter
+//
+//  Created by Brendan McNamara on 5/26/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import "TimeString.h"
+
+@implementation TimeString
+
+@end
