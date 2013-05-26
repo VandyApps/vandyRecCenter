65b3be2ea3f2ac37490f2a7da78a6e63727cce16
Brendan McNamara <brendan@streampiece.com>
2013-05-26 13:22:27 -0500

    implements loadData not yet tested

--- vandyRecCenter/GFModel.h
+++ vandyRecCenter/GFModel.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "VandyRecClient.h"
 
 @interface GFModel : NSObject
 
--- vandyRecCenter/GFModel.m
+++ vandyRecCenter/GFModel.m
@@ -8,6 +8,58 @@
 
 #import "GFModel.h"
 
+@interface GFModel()
+
+@property (nonatomic, strong) VandyRecClient* webClient;
+@end
+
 @implementation GFModel
 
+@synthesize webClient = _webClient;
+
+
+#pragma mark - getters
+- (VandyRecClient*) webClient {
+    if (_webClient == nil) {
+        _webClient = [[VandyRecClient alloc] init];
+    }
+    return _webClient;
+}
+
+#pragma mark - initializers
+
+- (id) init {
+    self = [super init];
+    return self;
+}
+
+#pragma mark load data
+
+- (void) loadData:(void (^)(NSError *, NSArray *))block forMonth:(NSInteger)month andYear:(NSInteger)year {
+    if (month < 0 || year < 0) {
+        self.month = -1;
+        self.year = -1;
+        [self.webClient JSONFromGFTab:^(NSError *error, NSArray *jsonData) {
+            if (error) {
+                block(error, jsonData);
+            } else {
+                self.GFClasses = jsonData;
+                block(nil, jsonData);
+            }
+        }];
+    } else {
+        self.month = month;
+        self.year = year;
+        [self.webClient JSONFromGFTab:^(NSError *error, NSArray *jsonData) {
+            if (error) {
+                block(error, jsonData);
+            } else {
+                self.GFClasses = jsonData;
+                block(nil, jsonData);
+            }
+        } forMonth: month andYear: year];
+    
+    }
+    
+}
 @end
