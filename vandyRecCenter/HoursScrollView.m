6538f1a308dac9ef6e79e5f91e1f2ff904f0a035
Brendan McNamara <brendan@streampiece.com>
2013-05-25 23:02:33 -0500

    adds logic to check for changes in frame

--- vandyRecCenter/HoursScrollView.m
+++ vandyRecCenter/HoursScrollView.m
@@ -8,7 +8,16 @@
 
 #import "HoursScrollView.h"
 
+@interface HoursScrollView()
+
+@property (nonatomic, assign) CGRect currentFrame;
+
+@end
 @implementation HoursScrollView
+
+
+
+#pragma mark - Initializers
 
 - (id)initWithFrame:(CGRect)frame
 {
@@ -22,14 +31,26 @@
 - (id) initWithCoder:(NSCoder *)aDecoder {
     self = [super initWithCoder: aDecoder];
     if (self) {
-        NSLog(@"initialization has taken place");
+        self.currentFrame = self.frame;
     }
     return self;
 }
+
+#pragma mark -Lifecycle
+
 - (void) layoutSubviews {
     
     [super layoutSubviews];
-    NSLog(@"layout subviews is called for the scroll view");
+    if (self.frame.size.height != self.currentFrame.size.height || self.frame.size.width != self.currentFrame.size.width || self.frame.origin.x != self.currentFrame.origin.x || self.frame.origin.y != self.currentFrame.origin.y) {
+        
+        self.currentFrame = self.frame;
+    }
 }
 
+
+#pragma mark- Publics
+
+- (void) setUpScrollViewWithHours:(NSDictionary *)hours {
+    //set up the scroll view here
+}
 @end
