1f14e3bd1d71c0201d1e1b226d46d3e5ab123a65
Brendan McNamara <brendan@streampiece.com>
2013-06-02 15:26:00 -0500

    creates GFSpecialDate file with method definitions

---
+++ vandyRecCenter/GFSpecialDates.m
@@ -0,0 +1,13 @@
+//
+//  GFSpecialDates.m
+//  vandyRecCenter
+//
+//  Created by Brendan McNamara on 6/2/13.
+//  Copyright (c) 2013 Brendan McNamara. All rights reserved.
+//
+
+#import "GFSpecialDates.h"
+
+@implementation GFSpecialDates
+
+@end
